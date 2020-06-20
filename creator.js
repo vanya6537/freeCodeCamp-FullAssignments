8192507625de02ffa8fa1de2041eb758e6e301c7
Netsl <vanya6537@gmail.com>
2020-06-20 12:38:13 +0400

    File generates directory for each section and javascript file for each subject

---
+++ creator.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+//Get all sections and themes from freeCodeCamp
+const {themes} = require('./constants')
+//For every section create a directory, for every subject create a javascript file
+Object.keys(themes).forEach((section) => {
+    console.log(section)
+    //If directory not exists - create it!
+    if (!fs.existsSync(section)) {
+        console.log('Not exist!')
+        fs.mkdirSync(section
+        );
+    }
+    //When directory is truly created - for every subject of this section we create separate javascript file
+    themes[section].forEach(subject => {
+        const filePath = `./${section}/${subject}.js`
+        fs.writeFile(filePath, '', {flag: 'wx'}, function (err) {
+            if (err) throw err;
+            console.log("It's saved!");
+        });
+    })
+})
