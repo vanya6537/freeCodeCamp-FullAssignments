81ecf6e887771ba7e5f699b9085853dd537856bd
Netsl <vanya6537@gmail.com>
2020-06-20 14:58:37 +0400

    Created empty files

---
+++ React/Render a Class Component to the DOM.js
@@ -0,0 +1,18 @@
+class TypesOfFood extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Types of Food:</h1>
+                <Fruits/>
+                <Vegetables/>
+            </div>
+        );
+    }
+}
+
+const targetNode = document.getElementById('challenge-node')
+ReactDOM.render(<TypesOfFood/>, targetNode)
